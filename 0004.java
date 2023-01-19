bdb6d80d6e9b2f0006af2af07d747d07e65b3665
Sujin Jeon <imxsuu@gmail.com>
2023-01-19 13:29:35 +0900

    feat: Solve 문자열 정렬하기(2)

---
+++ 0004.java
@@ -0,0 +1,15 @@
+# 문자열 정렬하기(2)
+#
+# 문제 설명
+# 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] str = my_string.toLowerCase().toCharArray();
+        Arrays.sort(str);
+        String answer = new String(str);
+        return answer;
+    }
+}
